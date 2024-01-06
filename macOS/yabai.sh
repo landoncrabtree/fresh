ae9eb3b3200b77f6c76c1e8ddad6d46d24f3ac93
Landon Crabtree <me@landon.pw>
2024-01-06 10:40:15 -0800

    Update yabai.sh

--- macOS/yabai.sh
+++ macOS/yabai.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-url=$(curl -s https://api.github.com/repos/koekeishiya/yabai/releases/latest \                                ─╯
-| grep "browser_download_url.*tar.gz" \
-| cut -d : -f 2,3 \
-| tr -d \" )
-url=$(echo "$url" | sed -e 's/^[[:space:]]*//')
-
-# Download the file using wget
-wget "$url" -O /tmp/yabai.tar.gz
-tar -xzvf /tmp/yabai.tar.gz
-sudo mv /tmp/archive/bin/yabai /usr/local/bin/yabai
+brew install koekeishiya/formulae/yabai
 
 # Setup yabai config
 mkdir -p ~/.config/yabai
@@ -17,9 +8,11 @@ wget https://raw.githubusercontent.com/landoncrabtree/fresh/main/macOS/.config/y
 chmod +x ~/.config/yabai/yabairc
 
 # Setup yabai launch agent
-sudo wget https://raw.githubusercontent.com/landoncrabtree/fresh/main/macOS/launchagents/pw.landon.yabai.plist -O ~/Library/LaunchAgents/pw.landon.yabai.plist
-sudo chown $(whoami):staff ~/Library/LaunchAgents/pw.landon.yabai.plist
-launchctl load ~/Library/LaunchAgents/pw.landon.yabai.plist
+# sudo wget https://raw.githubusercontent.com/landoncrabtree/fresh/main/macOS/launchagents/pw.landon.yabai.plist -O ~/Library/LaunchAgents/pw.landon.yabai.plist
+# sudo chown $(whoami):staff ~/Library/LaunchAgents/pw.landon.yabai.plist
+# launchctl load ~/Library/LaunchAgents/pw.landon.yabai.plist
+
+yabai --start-service
 
 # Setup skhd (keybinds for yabai)
 brew install koekeishiya/formulae/skhd
@@ -28,6 +21,8 @@ wget https://raw.githubusercontent.com/landoncrabtree/fresh/main/macOS/.config/s
 chmod +x ~/.config/skhd/skhdrc
 
 # Setup skhd launch agent
-sudo wget https://raw.githubusercontent.com/landoncrabtree/fresh/main/macOS/launchagents/pw.landon.skhd.plist -O ~/Library/LaunchAgents/pw.landon.skhd.plist
-sudo chown $(whoami):staff ~/Library/LaunchAgents/pw.landon.skhd.plist
-launchctl load ~/Library/LaunchAgents/pw.landon.skhd.plist
+# sudo wget https://raw.githubusercontent.com/landoncrabtree/fresh/main/macOS/launchagents/pw.landon.skhd.plist -O ~/Library/LaunchAgents/pw.landon.skhd.plist
+# sudo chown $(whoami):staff ~/Library/LaunchAgents/pw.landon.skhd.plist
+# launchctl load ~/Library/LaunchAgents/pw.landon.skhd.plist
+
+skhd --start-service
