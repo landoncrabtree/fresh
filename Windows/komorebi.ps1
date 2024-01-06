24d73235f2a05c90583d7ea1c79de90b6392f25d
Landon Crabtree <me@landon.pw>
2024-01-05 22:56:52 -0600

    Update komorebi.ps1

--- Windows/komorebi.ps1
+++ Windows/komorebi.ps1
@@ -11,6 +11,4 @@ iwr https://raw.githubusercontent.com/LGUG2Z/komorebi/v0.1.19/komorebi.example.j
 iwr https://raw.githubusercontent.com/LGUG2Z/komorebi/v0.1.19/whkdrc.sample -OutFile "$Env:USERPROFILE\.config\wkhd\whkdrc"
 
 komorebic fetch-app-specific-configuration
-
-Write-Host "DONE. Create a Scheduled Task with the following command arguments:"
-Write-Host "cmd.exe /C komorebic start -c ""$Env:USERPROFILE\.config\komorebi\komorebi.json"" --whkd"
+komorebic enable-autostart --config $Env:USERPROFILE\.config\komorebi\komorebi.json --whkd
