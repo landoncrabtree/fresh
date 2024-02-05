29f211a3eac23076807115312a44ca6786c92fb7
Landon Crabtree <me@landon.pw>
2024-02-05 14:43:16 -0800

    Create move.sh

---
+++ macOS/.config/skhd/move.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+space=$1
+
+current_spaces=$(yabai -m query --spaces | jq length)
+
+if [ $current_spaces -lt $space ]; then
+    new_space=$(($current_spaces + 1))
+    yabai -m space --create && yabai -m window --space $new_space && yabai -m space --focus $new_space
+else
+    yabai -m window --space $space && yabai -m space --focus $space
+fi
