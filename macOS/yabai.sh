624cba66606cbeb47f326b560c22f9c0c802942f
Landon Crabtree <me@landon.pw>
2024-04-13 21:14:57 -0700

    Update yabai.sh

--- macOS/yabai.sh
+++ macOS/yabai.sh
@@ -6,9 +6,6 @@ sudo nvram boot-args=-arm64e_preview_abi
 
 brew install koekeishiya/formulae/yabai
 
-# Setup yabai config
-mkdir -p ~/.config/yabai
-wget https://git.landon.pw/raw/fresh/macOS/.config/yabai/yabairc -O ~/.config/yabai/yabairc
 chmod +x ~/.config/yabai/yabairc
 
 # Setup yabai launch agent
@@ -19,9 +16,6 @@ chmod +x ~/.config/yabai/yabairc
 yabai --start-service
 
 # Setup skhd (keybinds for yabai)
-brew install koekeishiya/formulae/skhd
-mkdir -p ~/.config/skhd
-wget https://git.landon.pw/raw/fresh/macOS/.config/skhd/skhdrc -O ~/.config/skhd/skhdrc
 chmod +x ~/.config/skhd/skhdrc
 
 # Setup skhd launch agent
