12935925366bfd082714a7280a0db72721271516
Landon Crabtree <me@landon.pw>
2024-05-07 13:10:52 -0500

    Update yabai.sh

--- macOS/yabai.sh
+++ macOS/yabai.sh
@@ -5,7 +5,7 @@ sleep 10
 sudo nvram boot-args=-arm64e_preview_abi
 
 brew install koekeishiya/formulae/yabai
-
+brew install koekeishiya/formulae/skhd
 chmod +x ~/.config/yabai/yabairc
 
 # Setup yabai launch agent
@@ -13,14 +13,10 @@ chmod +x ~/.config/yabai/yabairc
 # sudo chown $(whoami):staff ~/Library/LaunchAgents/pw.landon.yabai.plist
 # launchctl load ~/Library/LaunchAgents/pw.landon.yabai.plist
 
+skhd --start-service
 yabai --start-service
-
-# Setup skhd (keybinds for yabai)
-chmod +x ~/.config/skhd/skhdrc
 
 # Setup skhd launch agent
 # sudo wget https://raw.githubusercontent.com/landoncrabtree/fresh/main/macOS/launchagents/pw.landon.skhd.plist -O ~/Library/LaunchAgents/pw.landon.skhd.plist
 # sudo chown $(whoami):staff ~/Library/LaunchAgents/pw.landon.skhd.plist
 # launchctl load ~/Library/LaunchAgents/pw.landon.skhd.plist
-
-skhd --start-service
