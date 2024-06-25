69739898c50520675283733032d64c231bf9c3b2
Landon Crabtree <me@landon.pw>
2024-06-24 19:25:29 -0500

    Update yabai.sh

--- macOS/yabai.sh
+++ macOS/yabai.sh
@@ -6,6 +6,7 @@ sudo nvram boot-args=-arm64e_preview_abi
 
 brew install koekeishiya/formulae/yabai
 brew install koekeishiya/formulae/skhd
+brew install hammerspoon --cask
 brew install jq
 
 chmod -R +x ~/.config/yabai/
@@ -20,6 +21,12 @@ echo "$(whoami) ALL=(root) NOPASSWD:$(which yabai) --load-sa" | sudo tee /privat
 
 skhd --start-service
 yabai --start-service
+
+git clone https://github.com/AdamWagner/stackline.git ~/.hammerspoon/stackline
+cd ~/.hammerspoon
+echo 'stackline = require "stackline"' >> init.lua
+echo 'stackline:init()' >> init.lua
+sed -i "s|/usr/local/bin/yabai|$(which yabai)|g" ~/.hammerspoon/stackline/conf.lua
 
 # Setup skhd launch agent
 # sudo wget https://raw.githubusercontent.com/landoncrabtree/fresh/main/macOS/launchagents/pw.landon.skhd.plist -O ~/Library/LaunchAgents/pw.landon.skhd.plist
