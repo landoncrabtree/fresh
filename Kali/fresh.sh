20f0fafe6e616b00b7bd3aeb6f733935aa759141
Landon Crabtree <me@landon.pw>
2024-06-27 19:45:59 -0500

    Update fresh.sh

--- Kali/fresh.sh
+++ Kali/fresh.sh
@@ -6,9 +6,14 @@ sudo git clone https://github.com/blindpentester/the-essentials.git
 cd the-essentials  
 sudo ./the_essentials.sh
 
+cd /opt
+sudo git clone https://github.com/Dewalt-arch/pimpmykali
+cd pimpmykali
+sudo ./pimpmykali.sh
+
 # Add aliases
-echo "[[ -f ~/.aliases ]] && source ~/.aliases" >> ~/.zshrc
-wget https://raw.githubusercontent.com/landoncrabtree/fresh/main/Kali/.aliases -O ->> ~/.aliases
+#echo "[[ -f ~/.aliases ]] && source ~/.aliases" >> ~/.zshrc
+#wget https://raw.githubusercontent.com/landoncrabtree/fresh/main/Kali/.aliases -O ->> ~/.aliases
 
 
 echo "DONE! Enjoy."
