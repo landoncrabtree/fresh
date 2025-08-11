438ec2ea48e95f6a8943b81bde3e3fd76ae27bcd
Landon Crabtree <me@landon.pw>
2025-08-10 20:51:35 -0500

    Update fresh.sh

--- Kali/fresh.sh
+++ Kali/fresh.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
+sudo apt update -y
+sudo apt upgrade -y
+
 # https://github.com/blindpentester/the-essentials
 cd /opt  
 sudo git clone https://github.com/blindpentester/the-essentials.git  
 cd the-essentials  
 sudo ./the_essentials.sh
 
+# PimpMyKali
 cd /opt
 sudo git clone https://github.com/Dewalt-arch/pimpmykali
 cd pimpmykali
 sudo ./pimpmykali.sh
 
-# Add aliases
-#echo "[[ -f ~/.aliases ]] && source ~/.aliases" >> ~/.zshrc
-#wget https://raw.githubusercontent.com/landoncrabtree/fresh/main/Kali/.aliases -O ->> ~/.aliases
+# OhMyZSH Stuff
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git $ZSH_CUSTOM/plugins/fast-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM}/themes/powerlevel10k
+sed -i '' 's/robbyrussell/powerlevel10k\/powerlevel10k/' ~/.zshrc
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions fast-syntax-highlighting)/' ~/.zshrc
+echo "[[ -f ~/.aliases ]] && source ~/.aliases" >> ~/.zshrc
 
+# Auto upgrade
+sudo apt install unattended-upgrades
+sudo dpkg-reconfigure --priority=low unattended-upgrades
 
 echo "DONE! Enjoy."
