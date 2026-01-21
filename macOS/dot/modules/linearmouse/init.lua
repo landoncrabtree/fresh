ffb52f08c8ef8defc0810ba730ad38ec98b16a60
Landon Crabtree <me@landon.pw>
2026-01-21 00:04:44 -0600

    Remove linearmouse

--- macOS/dot/modules/linearmouse/init.lua
+++
@@ -1,9 +0,0 @@
-return {
-    brew = { "linearmouse" },
-    config = {
-        {
-            source = "./linearmouse.json",
-            output = "~/.config/linearmouse/linearmouse.json",
-        },
-    },
-}
